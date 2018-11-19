e7d5eb892c298f33f8f910644a7d8f23a23ce3d6
Arjunr70 <45172488+Arjunr70@users.noreply.github.com>
2018-11-19 21:04:33 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+var app = angular .module("myModule", [])
+.controller("myController",['$scope','myService',function ($scope,myService) 
+{ $scope.orderByField = 'userId';
+$scope.reverseSort = false; 
+$scope.sortBy=function(field){ $scope.orderByField = field; $scope.reverseSort = !($scope.reverseSort); } myService.getData().then(function(response){ $scope.employees = response; }); /* $http.get('http://jsonplaceholder.typicode.com/posts').then(function(response) { $scope.employees = response.data;*/ }]) 
+.factory("myService", ['$http', function($http) { return { getData: function() { return $http.get('http://jsonplaceholder.typicode.com/posts') .then(function(response) { //return data when promise resolved that would help to continue promise chain. return response.data; }); } }; }]);
